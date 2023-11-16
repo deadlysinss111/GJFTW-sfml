b9a850e6824963ca10176ffcb194011987faf211
Riuck01 <tforest@gaming.tech>
2023-11-16 18:10:33 +0100

    fais un effort sur le constructeur brique Tom

--- GJFTW sfml/src/game objects/Brick.cpp
+++ GJFTW sfml/src/game objects/Brick.cpp
@@ -12,10 +12,7 @@ std::map<int, sf::Color> Brick::colorMap{ // Donne la couleur aux briques
 Brick::Brick(sf::RenderWindow* window, int x, int y, int hp) : GameObject(window, 100 + 121 * x, 20 + 42 * y, 120, 40) {
 	this->sprite.setOrigin(w / 2, h / 2);
 	this->sprite.setPosition(100 + 121 * x, 20 + 42 * y);
-	this->hp = hp;
-Brick::Brick(sf::RenderWindow* window, int x, int y, int hp) : GameObject(window, 100 +121 * x , 20 + 32 * y, 120, 30) {
 	this->hp = hp; // points de vie des briques
-	this->shape->setFillColor(this->colorMap.find(hp)->second);
 }
 
 
